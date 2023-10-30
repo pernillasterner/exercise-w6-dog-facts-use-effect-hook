9e0e99ae5dfd43dd45014dd1c66957eaa89ced62
Antonella Morittu <33493978+AntonellaMorittu@users.noreply.github.com>
2023-10-30 11:30:07 +0100

    Update DogFact.jsx explaining the meaning of clean data

--- src/components/DogFact.jsx
+++ src/components/DogFact.jsx
@@ -1,6 +1,7 @@
 // DogFact Component
 export const DogFact = () => {
-  // Hint: Extract the 'body' from 'attributes' of the 'fact' prop
+  // Hint: Extract the 'body' from 'attributes' of the 'fact' prop. We'll call this 'cleanFact'.
+  // Cleaning our data means choosing and keeping a somewhat small fraction of the data, the most useful data.
 
   // Hint: Render the 'cleanFact' if available, otherwise show a loading message
   return <div>dog fact here.</div>;
