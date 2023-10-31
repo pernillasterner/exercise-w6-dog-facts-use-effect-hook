b4475f0fd254e6ae232dea274ef05cc9a0bb3ea8
psterner <psterner@hotmail.com>
2023-10-31 11:54:25 +0100

    Use the useEffect Hook to fetch API

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,41 @@
 // Import required  hooks
+import { useEffect, useState } from "react";
 import { DogFact } from "./components/DogFact";
 
+/*
+1. Fetch the data from the Dog Facts free API JSON File and pass
+down the data from the parent component to its child component.
+*/
+
 export const App = () => {
+  const [dogFact, setDogFact] = useState("");
+  const [changeFact, setChangeFact] = useState(false);
+
   // Hint: Initialize state for storing the dog fact
   // Hint: Define the API endpoint
   // Hint: Create a function to fetch the dog fact
   // Hint: Use the useEffect hook to fetch the dog fact when the component mounts
+  const url = "https://dogapi.dog/api/v2/facts";
+
+  useEffect(() => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setDogFact(data);
+        console.log(data);
+      });
+  }, [changeFact]);
+
+  // const handleClick = () => {
+  //   setChangeFact(!changeFact);
+  // }
 
   return (
     <div className="App">
-      <DogFact />
+      {dogFact && <DogFact dogFact={dogFact} />}
+      <button onClick={() => setChangeFact(!changeFact)}>
+        Change dog fact
+      </button>
     </div>
   );
 };
--- src/components/DogFact.jsx
+++ src/components/DogFact.jsx
@@ -1,10 +1,12 @@
 // DogFact Component
-export const DogFact = () => {
+export const DogFact = ({ dogFact }) => {
   // Hint: Extract the 'body' from 'attributes' of the 'fact' prop. We'll call this 'cleanFact'.
+  const cleanFact = dogFact.data[0].attributes.body;
+
   // Cleaning our data means choosing and keeping a somewhat small fraction of the data, the most useful data.
 
   // Hint: Render the 'cleanFact' if available, otherwise show a loading message
-  return <div>dog fact here.</div>;
+  return <div className="cleanfact">{cleanFact}</div>;
 };
 
 // Hint: To use this component, import it in your main App component and pass the 'fact' prop to it.
